0d8a6e7f686c37fed406d5102576c876b680fe76
Daniel <dagranados@estudiantec.cr>
2023-05-02 20:26:34 -0600

    Avance CTEs

--- scripts/testScripts/QueryOptimize.sql
+++ scripts/testScripts/QueryOptimize.sql
@@ -434,7 +434,7 @@ GO
 -- Comparación inicio-final
 
 -- Inicio
-SET STATISTICS TIME ON;
+SET STATISTICS TIME,IO ON;
 
 SELECT facturas.facturaId AS [Factura.Id], facturas.fecha AS [Factura.Fecha],
 productores.productorId AS [Productor.Id], productores.nombre AS [Productor.Nombre], 
@@ -475,7 +475,7 @@ GROUP BY facturas.facturaId, facturas.fecha, actores.genericId, productores.prod
 ORDER BY [Factura.Id]
 FOR JSON PATH, ROOT ('Facturas');
 
-SET STATISTICS TIME OFF; 
+SET STATISTICS TIME, IO OFF; 
 GO
 
 -- V3
@@ -507,6 +507,25 @@ CREATE NONCLUSTERED INDEX IX_porcentajesActores_actorId
 ON porcentajesActores (actorId)
 INCLUDE (porcentaje, productoId)
 
+/*
+DROP INDEX [IX_itemsProductos_fecha] ON [dbo].[itemsProductos]
+GO
+
+DROP INDEX [IX_itemsFactura_tipoItemId] ON [dbo].[itemsFactura]
+GO
+
+DROP INDEX [IX_itemsFactura_facturaIdItemId] ON [dbo].[itemsFactura] WITH ( ONLINE = OFF )
+GO
+
+DROP INDEX [IX_lotesProduccionLogs_productoIdcontratoId] ON [dbo].[lotesProduccionLogs]
+GO
+
+DROP INDEX [IX_actoresContratoProd_objectTypeId] ON [dbo].[actoresContratoProd]
+GO
+
+DROP INDEX [IX_porcentajesActores_actorId] ON [dbo].[porcentajesActores]
+GO
+*/
 
 SET STATISTICS TIME, IO ON;
 
@@ -535,31 +554,73 @@ FOR JSON PATH, ROOT ('Facturas');
 SET STATISTICS TIME, IO OFF;
 GO
 
-/*
-DROP INDEX [IX_itemsProductos_fecha] ON [dbo].[itemsProductos]
+-- CTE encapsulada
+
+SET STATISTICS TIME, IO ON;
 GO
 
-DROP INDEX [IX_itemsFactura_tipoItemId] ON [dbo].[itemsFactura]
+WITH cteQuery ([Factura.Id], [Factura.Fecha], [Productor.Id], [Productor.Nombre], [Productor.CantidadProductosTotal],
+[Productor.DineroTotalProductor]) AS 
+(
+	SELECT facturas.facturaId AS [Factura.Id], facturas.fecha AS [Factura.Fecha],
+	productores.productorId AS [Productor.Id], productores.nombre AS [Productor.Nombre], 
+	SUM(items.cantidadProductos) AS [Productor.CantidadProductosTotal], 
+	SUM((items.montoTotal/tiposDeCambio.conversion) * porcentajes.porcentaje / 100) AS [Productor.DineroTotalProductor]
+	FROM facturas LEFT JOIN itemsFactura ON facturas.facturaId = itemsFactura.facturaId
+	INNER JOIN itemsProductos items ON items.itemProdId = itemsFactura.itemId
+	INNER JOIN lotesProduccionLogs lpl ON items.loteId = lpl.loteId
+	INNER JOIN contratosProduccion contProd ON contProd.prodContratoId = lpl.prodContratoId
+	INNER JOIN actoresContratoProd actores ON actores.prodContratoId = contProd.prodContratoId
+	INNER JOIN porcentajesActores porcentajes ON porcentajes.actorId = actores.actorId
+	RIGHT JOIN productores ON productores.productorId = actores.genericId
+	INNER JOIN tiposDeCambio ON tiposDeCambio.monedaCambioId = items.monedaId
+	WHERE items.fecha BETWEEN '2022-01-01 00:00:00' AND GETDATE() AND
+	itemsFactura.tipoItemId = 3 AND -- si es un item de venta de producto
+	actores.objectTypeId = 7 AND -- si el actor es un productor
+	porcentajes.productoId = lpl.productoId AND -- si el actor tiene un porcentaje del producto, participó en la producción de ese producto
+	lpl.productoId != 2 AND-- si la venta no involucra el producto 2.
+	productores.productorId != 5 -- se sustitye el except por una desigualdad
+	GROUP BY facturas.facturaId, facturas.fecha, actores.genericId, productores.productorId, productores.nombre
+)
+SELECT * FROM cteQuery
+	ORDER BY [Factura.Id]
+	FOR JSON PATH, ROOT ('Facturas')
+
+SET STATISTICS TIME, IO OFF;
 GO
 
-DROP INDEX [IX_itemsFactura_facturaIdItemId] ON [dbo].[itemsFactura] WITH ( ONLINE = OFF )
+
+
+SET STATISTICS TIME, IO ON;
+
+SELECT facturas.facturaId AS [Factura.Id], facturas.fecha AS [Factura.Fecha],
+productores.productorId AS [Productor.Id], productores.nombre AS [Productor.Nombre], 
+SUM(items.cantidadProductos) AS [Productor.CantidadProductosTotal], 
+SUM((items.montoTotal/tiposDeCambio.conversion) * porcentajes.porcentaje / 100) AS [Productor.DineroTotalProductor]
+FROM facturas LEFT JOIN itemsFactura ON facturas.facturaId = itemsFactura.facturaId
+INNER JOIN itemsProductos items ON items.itemProdId = itemsFactura.itemId
+INNER JOIN lotesProduccionLogs lpl ON items.loteId = lpl.loteId
+INNER JOIN contratosProduccion contProd ON contProd.prodContratoId = lpl.prodContratoId
+INNER JOIN actoresContratoProd actores ON actores.prodContratoId = contProd.prodContratoId
+INNER JOIN porcentajesActores porcentajes ON porcentajes.actorId = actores.actorId
+RIGHT JOIN productores ON productores.productorId = actores.genericId
+INNER JOIN tiposDeCambio ON tiposDeCambio.monedaCambioId = items.monedaId
+WHERE items.fecha BETWEEN '2022-01-01 00:00:00' AND GETDATE() AND
+itemsFactura.tipoItemId = 3 AND -- si es un item de venta de producto
+actores.objectTypeId = 7 AND -- si el actor es un productor
+porcentajes.productoId = lpl.productoId AND -- si el actor tiene un porcentaje del producto, participó en la producción de ese producto
+lpl.productoId != 2 AND-- si la venta no involucra el producto 2.
+productores.productorId != 5 -- se sustitye el except por una desigualdad
+GROUP BY facturas.facturaId, facturas.fecha, actores.genericId, productores.productorId, productores.nombre
+ORDER BY [Factura.Id], actores.genericId
+FOR JSON PATH, ROOT ('Facturas');
+
+SET STATISTICS TIME, IO OFF;
 GO
-
-DROP INDEX [IX_lotesProduccionLogs_productoIdcontratoId] ON [dbo].[lotesProduccionLogs]
-GO
-
-DROP INDEX [IX_actoresContratoProd_objectTypeId] ON [dbo].[actoresContratoProd]
-GO
-
-DROP INDEX [IX_porcentajesActores_actorId] ON [dbo].[porcentajesActores]
-GO
-*/
-
-
 
 -- CTE
 
-SET STATISTICS TIME ON;
+SET STATISTICS TIME, IO ON;
 
 WITH itemsProductosSort (itemProdId, loteId, cantidadProductos, montoTotal, monedaId) AS
 (
@@ -599,8 +660,9 @@ GROUP BY facturas.facturaId, facturas.fecha, actores.genericId, productores.prod
 ORDER BY [Factura.Id]
 FOR JSON PATH, ROOT ('Facturas');
 
-SET STATISTICS TIME OFF;
+SET STATISTICS TIME, IO OFF;
 GO
+
 /*
 USE [evtest]
 

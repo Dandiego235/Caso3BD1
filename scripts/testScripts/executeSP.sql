6390ab42490cbbd6eb54ef2b02a34499780180b5
Diego Granados <diegranados@estudiantec.cr>
2023-05-04 21:09:22 -0600

    fix sp

--- scripts/testScripts/executeSP.sql
+++ scripts/testScripts/executeSP.sql
@@ -6,11 +6,16 @@
 
 DECLARE @viajes AS viajesTabla;
 
-INSERT INTO @viajes VALUES (1), (24);
-
+INSERT INTO @viajes VALUES (1), (2);
+-- SET STATISTICS TIME ON;
 EXEC SP_registrarFacturaRecoleccion @viajes;
+-- SET STATISTICS TIME OFF;
 
 select * from facturas;
-select * from itemsFactura where facturaId =1002;
+select * from itemsFactura;
 select * from itemsRecoleccion;
 select * from saldosDistribucion;
+
+INSERT INTO [dbo].[itemsRecoleccion] ([productorId], [montoTotal], [recolectorId], [montoRec], [montoTrato], 
+		[montoComisionEV],[viajeId],[fechaFactura], [descuentoSaldo], [montoAPagar], [enabled], [createdAt], [computer],[username],[checksum])
+		VALUES (1,100.0,1, 10.0,10.0, 10.0, NULL, '2023-04-24 00:00:00', 10, 90, 1, '2023-04-24 10:00:00', 'ComputerName', 'Username', 0x0123456789ABCDEF)
